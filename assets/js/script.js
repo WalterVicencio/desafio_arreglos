aeeb1886e7a57bba5d521bb9c475e141a84a2d38
Walteriano <walter.8128@gmail.com>
2024-06-19 12:26:31 -0400

    empezando

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const propiedades_venta = [
+    {
+        nombre: 'Apartamento de lujo en zona exclusiva',
+        src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+        descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+        ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+        habitaciones: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'venta_1',
+        src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+        descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+        ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+        habitaciones: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    }
+]
+
+
+
+
+const propiedades_alquiler = [
+    {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    },
+]
+
+
+
+
+console.log(propiedades_alquiler[1])
